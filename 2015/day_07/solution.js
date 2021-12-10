840c764b5407d4271cb859b841dad6da6774d3ad
avrignaud <vrignaud.agathe@gmail.com>
2021-12-10 13:47:01 +0100

    2015 - day 07

---
+++ 2015/day_07/solution.js
@@ -0,0 +1,14 @@
+{/* Day 7 - 2021 */}
+
+const fs = require("fs")
+let input = fs
+    .readFileSync(__dirname + "/input", { encoding: "utf8" })
+    .split(/\r?\n/)
+
+// TODO : check binary operators in js
+
+{/* part 1 */}
+console.log('Day 7 - part 1: ')
+
+{/* part 2 */}
+console.log('Day 7 - part 2: ')
